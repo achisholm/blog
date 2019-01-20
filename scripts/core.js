b3919d7406bbd4038f21f7994ad23ed17516e0e1
Alistair <alistair@fastmail.com>
2019-01-20 19:04:32 +0000

    experimental layout

--- scripts/core.js
+++ scripts/core.js
@@ -2,6 +2,11 @@ const root = document.querySelector(':root');
 const rootStyles = getComputedStyle(root);
 const baseColor = rootStyles.getPropertyValue('--base-color');
 
+
+//let root = document.documentElement;
+root.style.setProperty('--base-color-h', Math.floor(Math.random() * 360) + 1);
+
+
 const menuBtn = document.getElementById('menu-button');
 const menuItems = document.querySelectorAll('.js-menu-item');
 
@@ -9,41 +14,41 @@ menuBtn.addEventListener("click", event => {
   document.body.classList.toggle("menu-active");
 });
 
-menuItems.forEach(item => {
-  item.addEventListener("click", event => {
-    let xhttp = new XMLHttpRequest();
-    let href = item.getAttribute("href");
+// menuItems.forEach(item => {
+//   item.addEventListener("click", event => {
+//     let xhttp = new XMLHttpRequest();
+//     let href = item.getAttribute("href");
 
-    xhttp.onreadystatechange = function () {
-      if (this.readyState == 4 && this.status == 200) {
+//     xhttp.onreadystatechange = function () {
+//       if (this.readyState == 4 && this.status == 200) {
 
-        let el = (domstring) => {
-          let stuff = new DOMParser().parseFromString(domstring, 'text/html');
-          return stuff.querySelector('.container__content').innerHTML;
-        }
-        document.querySelector(".container__content").innerHTML = el(this.response);
-        history.pushState(null, null, href);
-      }
-    };
-
-
-    xhttp.open("GET", href, true);
-    xhttp.send();
+//         let el = (domstring) => {
+//           let stuff = new DOMParser().parseFromString(domstring, 'text/html');
+//           return stuff.querySelector('.container__content').innerHTML;
+//         }
+//         document.querySelector(".container__content").innerHTML = el(this.response);
+//         history.pushState(null, null, href);
+//       }
+//     };
 
 
-
-
-    event.preventDefault();
-    event.stopPropagation();
-    return false;
-  });
+//     xhttp.open("GET", href, true);
+//     xhttp.send();
 
 
 
-})
+
+//     event.preventDefault();
+//     event.stopPropagation();
+//     return false;
+//   });
 
 
 
-document.addEventListener("DOMContentLoaded", function () {
+// })
 
-});
+
+
+// document.addEventListener("DOMContentLoaded", function () {
+
+// });
